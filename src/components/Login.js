078c03d2eed7d9755cc4d704665cbfc08371ff2c
SRIKA S <srithikasrika@gmail.com>
2025-12-01 14:47:40 +0530

    frontend

---
+++ src/components/Login.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import googleLogo from "../assets/googlelogo.jpg";
+import "./Login.css";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    if (email && password) {
+      localStorage.setItem("auth", "true");
+      navigate("/dashboard");
+    } else {
+      alert("Please enter Email & Password");
+    }
+  };
+
+  return (
+    <div className="login-container">
+
+      <div className="login-box">
+        <h2 className="title">SIGN IN</h2>
+
+        <button className="google-btn">
+  <img src={googleLogo} alt="google" />
+  Continue with Google
+</button>
+
+        <p className="or">or</p>
+
+        <h3 className="sub-title">LOGIN</h3>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="input-field"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="input-field"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="login-btn" onClick={handleLogin}>
+          Log in
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
